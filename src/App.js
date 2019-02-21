4f7f9b0b48b4b0a0aa881d175b31fc7d93bc8d5e
marcgregi <marc@hypedigital.co>
2019-02-21 16:50:55 +0000

    Adding Face Detect Model with Clarifai API and putting the image in an auto sized div.

--- src/App.js
+++ src/App.js
@@ -30,16 +30,19 @@ class App extends Component {
       super(); 
       this.state = {
         input: '', 
+        imageUrl: ''
       }
     }
 
     onInputChange = (event) => {
       console.log(event.target.value); 
+      this.setState({input: event.target.value})
     }
 
     onButtonSubmit = () => {
+      this.setState({imageUrl: this.state.input}); 
       console.log('click'); 
-      app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+      app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
     function(response) {
       console.log(response); 
       // do something with response
@@ -61,7 +64,7 @@ class App extends Component {
        <ImageLinkForm 
        onInputChange={this.onInputChange} 
        onButtonSubmit={this.onButtonSubmit} /> 
-       <FaceRecognition /> 
+       <FaceRecognition imageUrl={this.state.imageUrl} /> 
       </div>
     );
   }
